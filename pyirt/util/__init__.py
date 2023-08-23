6a1b52ba8c4fc516e002a7274ff1876e9ed47bb7
Ryutaro Mori <ryumorimori0609@gmail.com>
2023-08-23 17:27:33 +0900

    NGS-1076: Use forked pyirt repository via poetry

--- pyirt/util/__init__.py
+++ pyirt/util/__init__.py
@@ -2,7 +2,9 @@ __all__ = ["tools", "dao", "clib"]
 
 from . import tools
 from . import dao
+from os.path import expanduser
 
 import pyximport
-pyximport.install(build_dir="$HOME/.tmp/pyximport/", build_in_temp=True)
+home = expanduser("~")
+pyximport.install(build_dir=f"{home}/.tmp/pyximport/", build_in_temp=True)
 from . import clib
